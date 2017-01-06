fa6756bd3a7ab360dbe68bc1ee2ce20c93a44339
Verdi R-D <verdi@azend.org>
2017-01-06 16:03:13 -0500

    pull wordlist from github onstead of local file

--- invent.py
+++ invent.py
@@ -1,14 +1,20 @@
 from __future__ import print_function
 import markovify
 import click
+import requests
 
 @click.command()
 @click.option('--cards', default=1, help='Number of cards to generate')
 
 def invent(cards):
 
+    """
     with open('wordlist.txt') as f:
         text = f.read()
+    """
+
+    r = requests.get('https://raw.githubusercontent.com/azend/chains-invent-insanity/master/wordlist.txt')
+    text = r.text
 
     text_model = markovify.Text(text)
 
