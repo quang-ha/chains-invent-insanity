79e52b11351641d22dffa50ff116e3a0110c30df
Ryan Draga <ryan.draga@tuxotaku.com>
2017-01-06 16:33:31 -0500

    Add support for number of attempts Markovify makes at constructing a valid sentence

--- invent.py
+++ invent.py
@@ -5,8 +5,9 @@ import requests
 
 @click.command()
 @click.option('--cards', default=1, help='Number of cards to generate')
+@click.option('--attempts', default=10, help='How many times should the Markov Chain attempt to assemble a valid sentence. (Default is 10)')
 
-def invent(cards):
+def invent(cards, attempts):
 
     r = requests.get('https://s3.amazonaws.com/chains-invent-insanity/wordlist.txt')
     text = r.text
@@ -14,7 +15,7 @@ def invent(cards):
     text_model = markovify.Text(text)
 
     for i in range(cards):
-        print(text_model.make_sentence())
+        print(text_model.make_sentence(tries=attempts))
 
 if __name__ == '__main__':
     invent()
