1cc9ef9c0bd58f954f7f246d13b498582c36496a
Ryan Draga <ryan.draga@tuxotaku.com>
2017-01-21 18:08:17 -0500

    CH-CH-CH-CHAAANGES!!!

--- web.py
+++ web.py
@@ -1,25 +1,59 @@
-from flask import Flask, render_template
-from flask_wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, request
+from os.path import join, dirname
+from dotenv import load_dotenv, find_dotenv
+import os
 import markovify
 import requests
 #from libs import invent
 
+# Tell our app where to get its environment variables from
+dotenv_path = join(dirname(__file__), '.env')
+try:
+    load_dotenv(dotenv_path)
+except IOError:
+    find_dotenv()
+
 app = Flask(__name__)
+app.debug = os.environ.get('DEBUG_MODE')
+app.secret_key = os.environ.get('APP_KEY')
 
 
-@app.route("/")
-def invent():
+def invent(attempts, num_cards):
 
-    r = requests.get('https://s3.amazonaws.com/chains-invent-insanity/wordlist.txt')
-    text = r.text
+    use_local = os.environ.get('USE_LOCAL_WORDLIST')
+
+    if use_local is True:
+        local_wordlist = os.environ.get('LOCAL_WORDLIST')
+        text = local_wordlist
+    else:
+        remote_wordlist = os.environ.get('REMOTE_WORDLIST')
+        r = requests.get(remote_wordlist)
+        text = r.text
 
     text_model = markovify.Text(text)
 
-    for i in range(3):
-        return text_model.make_sentence(tries=100)
+    for i in range(num_cards):
+        return text_model.make_sentence(tries=attempts)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/form", methods=['GET', 'POST'])
+def form():
+
+    if request.method == 'POST':
+        num_cards = request.form['num_cards']
+        attempts = request.form['attempts']
+        return render_template('result.html', num_cards=num_cards, attempts=attempts)
+
+    return render_template('form.html')
+
+
+def main():
+    app.run()
 
 if __name__ == "__main__":
-    app.run()
+    main()
